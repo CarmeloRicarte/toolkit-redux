c3711dfd51ba391c20e997f6069faf8e961f8990
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-07 18:04:14 +0100

    feat: create store, slice for counter and use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import type { RootState } from "./store";
+import {
+  decrement,
+  decrementByAmount,
+  increment,
+  incrementByAmount,
+} from "./store/slices/counter";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { counter } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
@@ -15,10 +23,31 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Counter is: {counter}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <button
+          aria-label="Increment value plus 2"
+          onClick={() => dispatch(incrementByAmount(2))}
+        >
+          Increment + 2
+        </button>
+        <button
+          aria-label="Decrement value plus 2"
+          onClick={() => dispatch(decrementByAmount(2))}
+        >
+          Decrement - 2
         </button>
       </div>
     </div>
