3e8e1d6e030e76298258587991d1c2cf99aba913
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-12-07 20:58:05 +0100

    feat: show pokemons with pagination

--- src/PokemonApp.tsx
+++ src/PokemonApp.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useAppDispatch } from "./store/hooks";
 import { getPokemons } from "./store/slices/pokemon/thunks";
+import { useAppSelector } from "./store/hooks";
 
 export const PokemonApp = () => {
   const dispatch = useAppDispatch();
-
+  const pokemonState = useAppSelector((state) => state.pokemons);
+  const { isLoading, pokemons = [], page } = pokemonState;
   useEffect(() => {
     dispatch(getPokemons());
   }, []);
@@ -13,9 +15,17 @@ export const PokemonApp = () => {
     <>
       <h1>PokemonApp</h1>
       <hr />
+      <span>Loading: {isLoading ? "true" : "false"} </span>
       <ul>
-        <li>asdf</li>
+        {pokemons.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
       </ul>
+
+      <span>Current Page: {page}</span>
+      <button disabled={isLoading} onClick={() => dispatch(getPokemons(page))}>
+        Next page
+      </button>
     </>
   );
 };
